1267775f667042e55860387ae76a55d347106c5e
Paul Cowgill <pauldavidcowgill@gmail.com>
2020-02-15 19:14:00 -0700

    Added factory functionality

--- test/factory.ts
+++ test/factory.ts
@@ -1,12 +1,12 @@
-import { waffle } from '@nomiclabs/buidler';
+import {waffle} from '@nomiclabs/buidler';
 import chai from 'chai';
-import { deployContract, solidity } from 'ethereum-waffle';
+import {deployContract, solidity} from 'ethereum-waffle';
 
 import FactoryArtifact from '../build/contracts/FundingRoundFactory.json';
-import { FundingRoundFactory } from '../build/types/FundingRoundFactory';
+import {FundingRoundFactory} from '../build/types/FundingRoundFactory';
 
 chai.use(solidity);
-const { expect } = chai;
+const {expect} = chai;
 
 describe('Funding Round Factory', () => {
   const provider = waffle.provider;
@@ -16,109 +16,64 @@ describe('Funding Round Factory', () => {
   let factory: FundingRoundFactory;
 
   beforeEach(async () => {
-    factory = await deployContract(wallet, FactoryArtifact) as FundingRoundFactory;
+    factory = (await deployContract(
+      wallet,
+      FactoryArtifact
+    )) as FundingRoundFactory;
 
     expect(factory).to.properAddress;
   });
 
-  it('has new round running', async () => {
-    
-  });
+  it('has new round running', async () => {});
 
-  it('set contract owner/witness/coordinator correctly', async () => {
-    
-  });
+  it('set contract owner/witness/coordinator correctly', async () => {});
 
-  it('allows user to contribute to current round', async () => {
-    
-  });
+  it('allows user to contribute to current round', async () => {});
 
-  it('allows endRound to be called after round duration', async () => {
-    
-  });
+  it('allows endRound to be called after round duration', async () => {});
 
-  it('allows endRound to be called if new coordinator is set', async () => {
-    
-  });
+  it('allows endRound to be called if new coordinator is set', async () => {});
 
-  it('reverts if endRound is called and round not over or not newCoordinator', async () => {
-    
-  });
+  it('reverts if endRound is called and round not over or not newCoordinator', async () => {});
 
-  it('deploys new round and sets newRound to updated address when endRound is called', async () => {
-    
-  });
+  it('deploys new round and sets newRound to updated address when endRound is called', async () => {});
 
-  it('allows only witnesses to call setMaci', async () => {
-    
-  });
+  it('allows only witnesses to call setMaci', async () => {});
 
-  it('set MACI address correctly, and newMaci == true', async () => {
-    
-  });
+  it('set MACI address correctly, and newMaci == true', async () => {});
 
-  it('reverts if nextRound is called and newMaci != true', async () => {
-    
-  });
+  it('reverts if nextRound is called and newMaci != true', async () => {});
 
-  it('reverts if nextRound is called and coordinator == null', async () => {
-    
-  });
+  it('reverts if nextRound is called and coordinator == null', async () => {});
 
-  it('moves funds to current funding round when calling sendFundsToCurrentRound in previous round', async () => {
-    
-  });
+  it('moves funds to current funding round when calling sendFundsToCurrentRound in previous round', async () => {});
 
-  it('moves funds to current funding round when calling sendFundsToCurrentRound in current round', async () => {
-    
-  });
+  it('moves funds to current funding round when calling sendFundsToCurrentRound in current round', async () => {});
 
-  it('moves DAI balance of factory to current funding round when calling nextRound', async () => {
-    
-  });
+  it('moves DAI balance of factory to current funding round when calling nextRound', async () => {});
 
-  it('sets currentRound and newMaci correctly when nextRound is called', async () => {
-    
-  });
+  it('sets currentRound and newMaci correctly when nextRound is called', async () => {});
 
-  it('allows only the owner to call nextRound', async () => {
-    
-  });
+  it('allows only the owner to call nextRound', async () => {});
 
-  it('allows only the owner to set a new coordinator', async () => {
-    
-  });
+  it('allows only the owner to set a new coordinator', async () => {});
 
-  it('ends the round when setCoordinator is called', async () => {
-    
-  });
+  it('ends the round when setCoordinator is called', async () => {});
 
-  it('allows only the coordinator to call coordinatorQuit and sets coordinator to null', async () => {
-    
-  });
+  it('allows only the coordinator to call coordinatorQuit and sets coordinator to null', async () => {});
 
-  it('allows only the witness to call witnessQuit and sets witness to null and newMaci to false', async () => {
-    
-  });
+  it('allows only the witness to call witnessQuit and sets witness to null and newMaci to false', async () => {});
 
-  it('allows only the owner to call setOwner and sets new owner', async () => {
-    
-  });
+  it('allows only the owner to call setOwner and sets new owner', async () => {});
 
-  it('allows only the owner to call setWitness and sets new witness', async () => {
-    
-  });
+  it('allows only the owner to call setWitness and sets new witness', async () => {});
 
-  it('allows only the owner to call setRoundDuration and sets new round duration', async () => {
-    
-  });
+  it('allows only the owner to call setRoundDuration and sets new round duration', async () => {});
 
   it('allows any user to trigger a claim for themselves or others after a round is completed', async () => {
     // recipient receives contributions
     // recipient receives expected contribution amount
   });
 
-  it('checks that a MACI is valid when donations occur after round duration', async () => {
-    
-  });
+  it('checks that a MACI is valid when donations occur after round duration', async () => {});
 });
