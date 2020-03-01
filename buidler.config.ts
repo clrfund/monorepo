cbb5de1f54f8e870aeb6a8f937f4fd7b9308e180
Tommy Cox <tommy.cox@protonmail.com>
2020-02-29 23:57:52 -0700

    Add TypeChain support with example contract and test

--- buidler.config.ts
+++ buidler.config.ts
@@ -1,13 +1,19 @@
 import { BuidlerConfig, usePlugin } from "@nomiclabs/buidler/config";
 
 usePlugin("@nomiclabs/buidler-waffle");
+usePlugin("buidler-typechain");
 
 const config: BuidlerConfig = {
   paths: {
-    artifacts: "./build/artifacts"
+    artifacts: "./build/artifacts",
+    tests: "./tests/contracts"
   },
   solc: {
     version: "0.6.3"
+  },
+  typechain: {
+    outDir: "./build/types",
+    target: "ethers"
   }
 };
 
