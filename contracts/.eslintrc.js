dbb85470c62ee5b0ddf69aa0536ba6c2994bf40d
Max Goodman <max@maxgrok.com>
2020-06-30 11:26:28 -0400

    editing contract packages eslint

--- contracts/.eslintrc.js
+++ contracts/.eslintrc.js
@@ -4,27 +4,12 @@ module.exports = {
   plugins: [
     '@typescript-eslint',
   ],
-
-
-
-
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended"
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
   ],
-  overrides: [
-    {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)',
-      ],
-      env: {
-        mocha: true,
-      },
-    },
-  ],
+  env: {
+    mocha: true,
+    node: true,
+  },
 };
-
-
-
