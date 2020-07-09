4f3de772649902e4e675f27436a1e681f5959379
Kirill Goncharov <kdgoncharov@gmail.com>
2020-07-09 11:52:56 +0300

    Contracts: Document the RecipientRegistry interface

--- contracts/contracts/IRecipientRegistry.sol
+++ contracts/contracts/IRecipientRegistry.sol
@@ -1,6 +1,19 @@
 pragma solidity ^0.5.0;
 pragma experimental ABIEncoderV2;
 
+/**
+ * @dev Interface of the recipient registry.
+ *
+ * This contract must do the following:
+ *
+ * - Add recipients to the registry.
+ * - Allow only legitimate recipients into the registry.
+ * - Assign an unique index to each recipient.
+ * - Find the recipient's index by their address.
+ * - Limit the maximum number of entries according to MACI's maxVoteOptions.
+ * - (TODO) Remove invalid entries.
+ * - (TODO) Prevent indices from changing during the funding round.
+ */
 interface IRecipientRegistry {
 
   function addRecipient(address _fundingAddress, string calldata _name) external;
