05746f8867f9b1da0a7df55db607783c6a63a1d0
Kirill Goncharov <kdgoncharov@gmail.com>
2020-07-26 00:40:22 +0300

    Frontend: Remove extra files and e2e tests

--- vue-app/tests/unit/example.spec.ts
+++ vue-app/tests/unit/example.spec.ts
@@ -1,13 +1,12 @@
-import { expect } from "chai";
-import { shallowMount } from "@vue/test-utils";
-import HelloWorld from "@/components/HelloWorld.vue";
+import { expect } from 'chai'
+import { shallowMount } from '@vue/test-utils'
+import ProjectItem from '@/components/ProjectItem.vue'
 
-describe("HelloWorld.vue", () => {
-  it("renders props.msg when passed", () => {
-    const msg = "new message";
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    });
-    expect(wrapper.text()).to.include(msg);
-  });
-});
+describe('ProjectItem.vue', () => {
+  it('renders props.msg when passed', () => {
+    const wrapper = shallowMount(ProjectItem, {
+      propsData: { },
+    })
+    expect(wrapper.text()).to.include('')
+  })
+})
