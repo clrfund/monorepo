f2b36ccf735cd4e293f44812660a54c55932e2ee
Kirill Goncharov <kdgoncharov@gmail.com>
2020-09-19 20:59:29 +0300

    Frontend: Define Tally interface

--- vue-app/src/api/tally.ts
+++ vue-app/src/api/tally.ts
@@ -3,7 +3,9 @@ import { Contract } from 'ethers'
 import { FundingRound } from './abi'
 import { provider, ipfsGatewayUrl } from './core'
 
-export async function getTally(fundingRoundAddress: string): Promise<any> {
+import { Tally } from '@/utils/maci'
+
+export async function getTally(fundingRoundAddress: string): Promise<Tally> {
   const fundingRound = new Contract(fundingRoundAddress, FundingRound, provider)
   const tallyHash = await fundingRound.tallyHash()
   const response = await fetch(`${ipfsGatewayUrl}${tallyHash}`)
