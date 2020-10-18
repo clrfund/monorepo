b624883ce6ac0b797db6a1433543585d30aa59ad
Kirill Goncharov <kdgoncharov@gmail.com>
2020-10-18 19:35:30 +0300

    Contracts: Remove extra imports in MACIFactory contract

--- contracts/buidler.config.ts
+++ contracts/buidler.config.ts
@@ -50,6 +50,17 @@ task('compile', 'Compiles the entire project, building all artifacts', async (_,
   // Copy Poseidon artifacts to target directory
   fs.copyFileSync('../node_modules/maci-contracts/compiled/PoseidonT3.json', path.join(config.paths.artifacts, 'PoseidonT3.json'));
   fs.copyFileSync('../node_modules/maci-contracts/compiled/PoseidonT6.json', path.join(config.paths.artifacts, 'PoseidonT6.json'));
+  // Prepare verifier artifacts for 'test' circuits
+  const verifiers = ['BatchUpdateStateTreeVerifier', 'QuadVoteTallyVerifier']
+  for (const contractName of verifiers) {
+    const abi = JSON.parse(fs.readFileSync(`../node_modules/maci-contracts/compiled/${contractName}.abi`).toString())
+    const bytecode = fs.readFileSync(`../node_modules/maci-contracts/compiled/${contractName}.bin`).toString()
+    const artifact = { contractName, abi, bytecode }
+    fs.writeFileSync(
+      path.join(config.paths.artifacts, `${contractName}.json`),
+      JSON.stringify(artifact),
+    )
+  }
 });
 
 export default config
