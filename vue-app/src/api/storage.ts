c04b4d3a5682d9db6277f73469c1eee073fa2a91
Kirill Goncharov <kdgoncharov@gmail.com>
2021-01-24 22:30:01 +0300

    Frontend: Change storage key structure

--- vue-app/src/api/storage.ts
+++ vue-app/src/api/storage.ts
@@ -2,24 +2,21 @@ import { sha256, encrypt, decrypt } from '@/utils/crypto'
 
 function getFullStorageKey(
   accountId: string,
-  storageId: string,
   storageKey: string,
 ): string {
-  storageId = storageId.toLowerCase()
   accountId = accountId.toLowerCase()
-  return sha256(`clrfund-${storageId}-${accountId}-${storageKey}`)
+  return sha256(`clrfund-${accountId}-${storageKey}`)
 }
 
 function setItem(
   accountId: string,
   encryptionKey: string,
-  storageId: string,
   storageKey: string,
   value: string,
 ): void {
   const encryptedValue = encrypt(value, encryptionKey)
   window.localStorage.setItem(
-    getFullStorageKey(accountId, storageId, storageKey),
+    getFullStorageKey(accountId, storageKey),
     encryptedValue,
   )
 }
@@ -27,11 +24,10 @@ function setItem(
 function getItem(
   accountId: string,
   encryptionKey: string,
-  storageId: string,
   storageKey: string,
 ): string | null {
   const encryptedValue = window.localStorage.getItem(
-    getFullStorageKey(accountId, storageId, storageKey),
+    getFullStorageKey(accountId, storageKey),
   )
   const value = encryptedValue ? decrypt(encryptedValue, encryptionKey) : null
   return value
