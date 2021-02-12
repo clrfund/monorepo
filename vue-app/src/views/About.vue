4b35eddd482a2fe66df02429cb94842b92dfc40e
Auryn Macmillan <aurynmacmillan@gmail.com>
2021-02-12 08:55:33 -0500

    Update about page.

--- vue-app/src/views/About.vue
+++ vue-app/src/views/About.vue
@@ -14,12 +14,14 @@
     <p>The first ~200 ETHDenver attendees to contribute to the QF round get to help decide how the matching pool should be allocated by contributing xDai to their favorite projects. Just make sure to log in with the Ethereum address you provided in the ETHDenver signup form.</p>
 
     <h2>How do I vote?</h2>
-    <p>To vote, simply <a href="https://www.xdaichain.com/for-users/wallets/metamask/metamask-setup" target="_blank" rel="noopener">connect your metamask to xDai</a>, hit the "contribute" button next to any projects that you want to vote for, and allocate any amount of xDai to each of the projects you selected. Remember, your vote weight for each project is the square root of the amount of xDai you contribute to that project.</p>
+    <p>To vote, simply <a href="https://www.xdaichain.com/for-users/wallets/metamask/metamask-setup" target="_blank" rel="noopener">connect your metamask to xDai</a>, hit the "contribute" button next to any projects that you want to vote for, and allocate any amount of wxDai to each of the projects you selected. Remember, your vote weight for each project is the square root of the amount of wxDai you contribute to that project.</p>
 
-    <h2>Where can I get xDai?</h2>
+    <h2>Where can I get wxDai?</h2>
     <p>Don't have any xDai yet? No worries.</p>
     <p>If you have Dai on mainnet, you can bridge it over to xDai using the <a href="https://bridge.xdaichain.com/" target="_blank" rel="noopener">xDai bridge</a></p>
     <p>Alternatively, you can buy xDai with a credit/debit card on <a href="https://honeyswap.org/" target="_blank" rel="noopener">Honeyswap</a>
+    <p>You'll need this xDai to pay gas on the xDai network. But don't worry, transactions are much cheaper than on mainnet. 1 xDai should be a lifetime supply of gas for most users.</p>
+    <p>To contribute to the round, you'll need to wrap some of your xDai into wxDai (similar to how you can wrap ETH into WETH on mainnet). You can do this on back on <a href="https://honeyswap.org/" target="_blank" rel="noopener">Honeyswap</a> or over at <a href="http://wrapeth.com/" target="_blank" rel="noopener">Wrapeth</a>.
 
     <h2>What's under the hood?</h2>
     <p>ETHDenver's Quadratic Funding round is powered by <a href="https://clr.fund" target="_blank" rel="noopener">clr.fund</a>, a QF protocol built on Ethereum using Zero Knowledge Proofs (courtesy of <a href="https://github.com/appliedzkp/maci" target="_blank" rel="noopener">MACI</a>).</p>
