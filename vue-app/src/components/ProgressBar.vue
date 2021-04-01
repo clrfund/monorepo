de91366b8250c71bc5c2c47cab2f6eaf15fdc185
Sam Richards <sbrichards@gmail.com>
2021-03-31 19:17:29 -0700

    Add simple ProgressBar component

---
+++ vue-app/src/components/ProgressBar.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="progress-bar">
+    <div v-for="idx in totalSteps" v-bind:key="idx">
+      <div class="step" :class="{'inactive': isFutureStep(idx)}"/>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+import { Prop } from 'vue-property-decorator'
+
+@Component
+export default class Transaction extends Vue {
+  @Prop()
+  currentStep!: number
+
+  @Prop()
+  totalSteps!: number
+
+  // TODO best way to pass props to CSS?
+  isFutureStep (step) {
+    return step > this.currentStep
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@import '../styles/vars';
+
+.progress-bar {
+  color: $error-color;
+  margin: 32px 0;
+  display: flex;
+  justify-content: space-between;
+}
+
+.step {
+  background-color: pink;
+  height: 8px;
+  width: 48px;
+
+}
+.inactive {
+  background-color: purple;
+}
+
+</style>
--- vue-app/src/views/Apply.vue
+++ vue-app/src/views/Apply.vue
@@ -4,13 +4,33 @@
       ⟵ Back
     </a>
     <div class="application-page">
-      Application page?
-      <p>
-        currentStep: {{currentStep}}
-      </p>
-      <router-link :to="{ name: 'apply', params: { step: steps[currentStep + 1] }}">Previous step</router-link>
-      <br>
-      <router-link :to="{ name: 'apply', params: { step: steps[currentStep + 1] }}">Next step</router-link>
+      <progress-bar :currentStep="currentStep + 1" :totalSteps="steps.length"/>
+      <router-link v-if="currentStep > 0" :to="{ name: 'apply', params: { step: steps[currentStep - 1] }}">Previous step</router-link>
+      <h3>
+        Step {{currentStep + 1}} of {{steps.length}}
+      </h3>
+
+      <form>
+
+        <div v-if="currentStep === 0">
+          <h2>Checking BrightID verification...</h2>
+        </div>
+        <div v-if="currentStep === 1">
+          <h2>Doing something new...</h2>
+        </div>
+        <div v-if="currentStep === 2">
+          <h2>Doing something dumb...</h2>
+        </div>
+        <div v-if="currentStep === 3">
+          <h2>Doing something fun...</h2>
+        </div>
+        <div v-if="currentStep === 4">
+          <h2>Yay almost done...</h2>
+        </div>
+
+      </form>
+
+      <router-link v-if="currentStep < 4" :to="{ name: 'apply', params: { step: steps[currentStep + 1] }}">Next step</router-link>
     </div>
   </div>
 </template>
@@ -19,9 +39,14 @@
 import Vue from 'vue'
 import Component from 'vue-class-component'
 
+import ProgressBar from '@/components/ProgressBar.vue'
+
 @Component({
   name: 'ApplyView',
   metaInfo: { title: 'Apply' },
+  components: {
+    ProgressBar,
+  },
 })
 export default class About extends Vue { 
 
@@ -31,8 +56,14 @@ export default class About extends Vue {
   created() {
     const steps = ['one', 'two', 'three', 'four', 'five']
     const currentStep = steps.indexOf(this.$route.params.step)
-    this.currentStep = currentStep
     this.steps = steps
+    this.currentStep = currentStep
+    // TODO redirect to /apply/one if step doesn't exist
+    // if (currentStep < 0) {
+    //   console.log('NO STEP')
+    //   this.$router.push({ name: 'apply', params: 'one' })
+    //   return
+    // }
   }
 }
 </script>
