2b7ed4debc120a64070f1e8fb096d2e8c7e1f69f
Sam Richards <sbrichards@gmail.com>
2021-03-31 18:29:02 -0700

    Create basic Apply view

--- vue-app/src/router/index.ts
+++ vue-app/src/router/index.ts
@@ -7,6 +7,7 @@ import ProjectList from '../views/ProjectList.vue'
 import ProjectView from '../views/Project.vue'
 import RoundList from '../views/RoundList.vue'
 import About from '../views/About.vue'
+import ApplyVue from '../views/Apply.vue'
 import RecipientRegistryView from '@/views/RecipientRegistry.vue'
 
 Vue.use(VueRouter)
@@ -41,6 +42,11 @@ const routes = [
     path: '/about',
     name: 'about',
     component: About,
+  },
+  {
+    path: '/apply/:step',
+    name: 'apply',
+    component: ApplyVue,
   },
   {
     path: '/recipients',
