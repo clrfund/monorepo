f65920c8a1bcb734fa6ae5cf22516803843d74ed
Ryan Cordell <ryancordell@virginmedia.com>
2021-05-19 15:32:55 +0100

    added cart indicator
    Can't work out how to only get it to show on cart tab

--- vue-app/src/components/MobileTabs.vue
+++ vue-app/src/components/MobileTabs.vue
@@ -9,11 +9,19 @@
       }"
       :to="to"
     >
+    <div class="icon">
       <img
         :src="require(`@/assets/${icon}`)"
         :alt="title"
         width="16px"
       > 
+      <div 
+        class="cart-indicator" 
+        v-if="!isCartEmpty" 
+      >
+        {{ cartNumber }}
+      </div>
+    </div>
       <span class="tab-title">{{ title }}</span>
     </router-link>
   </div>
@@ -23,6 +31,8 @@
 import Vue from 'vue'
 
 export default class MobileTabs extends Vue {
+  isCartEmpty = false
+  cartNumber = '8'
   tabs = [
     {
       icon: 'timer.svg',
@@ -49,6 +59,9 @@ export default class MobileTabs extends Vue {
 </script>
 
 <style lang="scss" scoped>
+@import '../styles/vars';
+@import '../styles/theme';
+
 .tab-container {
   position: fixed;
   bottom: 0;
@@ -73,6 +86,26 @@ export default class MobileTabs extends Vue {
   line-height: 0;
   margin-top: -0.75rem;
   font-size: 14px;
+  text-transform: uppercase;
+  color: #fff;
+}
+
+.cart-indicator {
+  border-radius: 2rem;
+  background: $clr-pink-light-gradient;
+  padding: 0.25rem;
+  font-size: 10px;
+  color: #fff;
+  line-height: 100%;
+  width: 8px;
+  height: 8px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.icon {
+  display: flex;
 }
 
 .active {
