58ba5cb9213de14397ad3b7b6fe44bdfc9577e05
Sam Richards <sbrichards@gmail.com>
2021-05-31 19:36:26 -0700

    Owners can execute requests during challenge period [Fixes #344]

--- contracts/tests/recipientRegistry.ts
+++ contracts/tests/recipientRegistry.ts
@@ -622,12 +622,27 @@ describe('Optimistic recipient registry', () => {
         .to.be.revertedWith('RecipientRegistry: Request does not exist')
     })
 
-    it('should not allow to execute request during challenge period', async () => {
+    it('should not allow non-owner to execute request during challenge period', async () => {
       await registry.addRecipient(
         recipientAddress, metadata, { value: baseDeposit },
       )
-      await expect(registry.executeRequest(recipientId))
+
+      await expect(registry.connect(requester).executeRequest(recipientId))
         .to.be.revertedWith('RecipientRegistry: Challenge period is not over')
+    })
+
+    it('should allow owner to execute request during challenge period', async () => {
+      await registry.addRecipient(
+        recipientAddress, metadata, { value: baseDeposit },
+      )
+
+      let recipientCount = await registry.getRecipientCount()
+      expect(recipientCount.toNumber()).to.equal(0)
+
+      await registry.executeRequest(recipientId)
+
+      recipientCount = await registry.getRecipientCount()
+      expect(recipientCount.toNumber()).to.equal(1)
     })
 
     it('should remember initial deposit amount during registration', async () => {
