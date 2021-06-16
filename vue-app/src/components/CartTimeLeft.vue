66a4439a6ac86a13cc221aab56d1b1d1a3655678
Sam Richards <sbrichards@gmail.com>
2021-06-16 12:53:14 -0700

    Simplify CartTimeLeft

--- vue-app/src/components/CartTimeLeft.vue
+++ vue-app/src/components/CartTimeLeft.vue
@@ -1,21 +1,13 @@
 <template>
   <div>
     <div class="flex"><img src="@/assets/time.svg" />Time left</div>
-    <div v-if="$store.getters.canUserReallocate" class="flex">
-      <div v-if="reallocationTimeLeft.days > 0">{{ reallocationTimeLeft.days }}</div>
-      <div v-if="reallocationTimeLeft.days > 0">days</div>
-      <div>{{ reallocationTimeLeft.hours }}</div>
+    <div class="flex">
+      <div v-if="timeLeft.days > 0">{{ timeLeft.days }}</div>
+      <div v-if="timeLeft.days > 0">days</div>
+      <div>{{ timeLeft.hours }}</div>
       <div>hours</div>
-      <div v-if="reallocationTimeLeft.days === 0">{{ reallocationTimeLeft.minutes }}</div>
-      <div v-if="reallocationTimeLeft.days === 0">minutes</div>
-    </div>
-    <div v-else class="flex">
-      <div v-if="contributionTimeLeft.days > 0">{{ contributionTimeLeft.days }}</div>
-      <div v-if="contributionTimeLeft.days > 0">days</div>
-      <div>{{ contributionTimeLeft.hours }}</div>
-      <div>hours</div>
-      <div v-if="contributionTimeLeft.days === 0">{{ contributionTimeLeft.minutes }}</div>
-      <div v-if="contributionTimeLeft.days === 0">minutes</div>
+      <div v-if="timeLeft.days === 0">{{ timeLeft.minutes }}</div>
+      <div v-if="timeLeft.days === 0">minutes</div>
     </div>
   </div>
 </template>
@@ -23,7 +15,6 @@
 <script lang="ts">
 import Vue from 'vue'
 import Component from 'vue-class-component'
-import { Prop } from 'vue-property-decorator'
 
 import { TimeLeft } from '@/api/round'
 import { getTimeLeft } from '@/utils/dates'
@@ -36,6 +27,10 @@ export default class CartTimeLeft extends Vue {
 
   get reallocationTimeLeft(): TimeLeft {
     return getTimeLeft(this.$store.state.currentRound.votingDeadline)
+  }
+
+  get timeLeft(): TimeLeft {
+    return this.$store.getters.canUserReallocate ? this.reallocationTimeLeft : this.contributionTimeLeft
   }
 }
 </script>
