7a372efc720d356d657859b73ac816706c51159f
Sam Richards <sbrichards@gmail.com>
2021-06-19 15:36:30 -0700

    Dispatch SAVE_CART action

--- vue-app/src/components/Cart.vue
+++ vue-app/src/components/Cart.vue
@@ -222,7 +222,7 @@ export default class Cart extends Vue {
   
   removeAll(): void {
     this.$store.commit(CLEAR_CART)
-    this.$store.commit(SAVE_CART)
+    this.$store.dispatch(SAVE_CART)
     this.$store.commit(TOGGLE_EDIT_SELECTION, true)
   }
 
