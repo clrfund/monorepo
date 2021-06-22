aea556ed4338b806782ed5221ef94364039cbd49
Pablo <pettinarip@gmail.com>
2021-06-22 12:00:10 -0300

    add prettier

--- contracts/.eslintrc.js
+++ contracts/.eslintrc.js
@@ -1,31 +1,18 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
-  plugins: [
-    '@typescript-eslint',
-  ],
+  plugins: ['@typescript-eslint'],
   extends: [
     'eslint:recommended',
-    "plugin:@typescript-eslint/eslint-recommended",
+    'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
+    'prettier',
   ],
   env: {
     mocha: true,
     node: true,
   },
   rules: {
-    'indent': [
-      'error',
-      2,
-    ],
-    'comma-dangle': [
-      'error',
-      'always-multiline',
-    ],
-    'quotes': [
-      'error',
-      'single',
-    ],
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
   },
