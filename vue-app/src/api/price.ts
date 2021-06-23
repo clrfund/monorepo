25a30520b416dcc109a7d6037b5c28775bede2ca
Corwintines <cssmittys@gmail.com>
2021-06-23 14:58:57 -0600

    chore: prettier

--- vue-app/src/api/price.ts
+++ vue-app/src/api/price.ts
@@ -1,11 +1,13 @@
 export interface EthPrice {
   ethereum: {
-    usd: number;
-  };
+    usd: number
+  }
 }
 
 export async function fetchCurrentEthPrice(): Promise<EthPrice> {
-  const res = await fetch('https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd')
+  const res = await fetch(
+    'https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd'
+  )
   const data = await res.json()
   return data
 }
