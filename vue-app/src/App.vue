a3e819d4696e6a76aecedb0c6d5927388620cc84
Corwin Smith <cssmittys@gmail.com>
2021-06-25 08:25:46 -0600

    Update App.vue

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -80,8 +80,8 @@ export default class App extends Vue {
     setInterval(() => {
       this.$store.dispatch(LOAD_ROUND_INFO)
     }, 60 * 1000)
-    setInterval(() => {
-      this.$store.dispatch(LOAD_RECIPIENT_REGISTRY_INFO)
+    setInterval(async () => {
+      await this.$store.dispatch(LOAD_RECIPIENT_REGISTRY_INFO)
     }, 60 * 1000)
     setInterval(() => {
       this.$store.dispatch(LOAD_USER_INFO)
