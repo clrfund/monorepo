b8442934f4f7a283588c8e75989d4636dd8b43fe
wackerow <paul.wackerow@ethereum.org>
2021-07-08 23:22:47 -0400

    Q

---
+++ vue-app/src/components/BackToProjects.vue
@@ -0,0 +1,36 @@
+<template>
+  <router-link
+    to="/projects"
+    :class="{
+      back: true,
+      desktop: !alsoShowOnMobile,
+    }"
+    >← Back to projects</router-link
+  >
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+import { Prop } from 'vue-property-decorator'
+
+@Component
+export default class extends Vue {
+  @Prop() alsoShowOnMobile!: boolean
+}
+</script>
+
+<style lang="scss" scoped>
+.back {
+  position: relative;
+  display: flex;
+  flex: 1 0 auto;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 1rem;
+  color: #fff;
+  &:hover {
+    opacity: 0.8;
+  }
+}
+</style>
