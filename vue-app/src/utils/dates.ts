436c8a9a3cf2f8a669a1ad1bd535cbda641c7cf4
Corwintines <cssmittys@gmail.com>
2021-07-13 13:45:21 -0600

    fix: hours and minutes conversion takes into account 60 minutes in the Math.ceil now

--- vue-app/src/utils/dates.ts
+++ vue-app/src/utils/dates.ts
@@ -27,7 +27,11 @@ export function getTimeLeft(date: DateTime): TimeLeft {
     return { days: 0, hours: 0, minutes: 0 }
   }
   const { days, hours, minutes } = date.diff(now, ['days', 'hours', 'minutes'])
-  return { days, hours, minutes: Math.ceil(minutes) }
+  return {
+    days,
+    hours: days !== 0 ? hours : Math.ceil(minutes) % 60 ? hours : hours + 1, // If days are 0, and minutes are 60, add 1 to the hours to display properly
+    minutes: Math.ceil(minutes) % 60 ? Math.ceil(minutes) : 0, // If minutes are 60, carry that over to the hours above and show 0
+  }
 }
 
 // TODO handle dates in the past (difference < 0, returns '0 seconds')
