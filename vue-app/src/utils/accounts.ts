6da61d232b04b28b30a590d8ac390c3006141e65
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2021-08-30 17:52:32 -0700

    move comments above function declaration

--- vue-app/src/utils/accounts.ts
+++ vue-app/src/utils/accounts.ts
@@ -6,21 +6,21 @@ export function isSameAddress(address1: string, address2: string): boolean {
   return ethers.utils.getAddress(address1) === ethers.utils.getAddress(address2)
 }
 
+// Looks up possible ENS for given 0x address
 export async function ensLookup(address: string): Promise<string | null> {
-  // Looks up possible ENS for given 0x address
   const name: string | null = await mainnetProvider.lookupAddress(address)
   return name
 }
 
+// Returns null if the name passed is a 0x address
+// If name is valid ENS returns 0x address, else returns null
 export async function resolveEns(name: string): Promise<string | null> {
-  // Returns null if the name passed is a 0x address
   if (isAddress(name)) return null
-  // If name is valid ENS returns 0x address, else returns null
   return await mainnetProvider.resolveName(name)
 }
 
+// Returns true if address is valid ENS or 0x address
 export async function isValidEthAddress(address: string): Promise<boolean> {
-  // Returns true if address is valid ENS or 0x address
   const resolved = await mainnetProvider.resolveName(address)
   return !!resolved
 }
