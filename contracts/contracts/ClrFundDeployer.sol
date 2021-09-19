952fb0b6dbbc6b6739b6b53668c700e141b74d6d
viraj124 <virajm72@gmail.com>
2021-09-19 22:29:47 +0530

    feat: setup oz proxy for maci factory with test coverage

--- contracts/contracts/ClrFundDeployer.sol
+++ contracts/contracts/ClrFundDeployer.sol
@@ -22,19 +22,7 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 pragma solidity ^0.6.12;
 import './MACIFactory.sol';
 import './ClrFund.sol';
-
-contract CloneFactory { // implementation of eip-1167 - see https://eips.ethereum.org/EIPS/eip-1167
-    function createClone(address target) internal returns (address result) {
-        bytes20 targetBytes = bytes20(target);
-        assembly {
-            let clone := mload(0x40)
-            mstore(clone, 0x3d602d80600a3d3981f3363d3d373d3d3d363d73000000000000000000000000)
-            mstore(add(clone, 0x14), targetBytes)
-            mstore(add(clone, 0x28), 0x5af43d82803e903d91602b57fd5bf30000000000000000000000000000000000)
-            result := create(0, clone, 0x37)
-        }
-    }
-}
+import "./CloneFactory.sol";
 
 contract ClrFundDeployer is CloneFactory { 
     
---
+++ contracts/contracts/ClrFundMACIFactory.sol
@@ -0,0 +1,148 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.6.12;
+pragma experimental ABIEncoderV2;
+
+import './OwnableUpgradeable.sol';
+import 'maci-contracts/sol/MACI.sol';
+import 'maci-contracts/sol/MACIParameters.sol';
+import 'maci-contracts/sol/MACISharedObjs.sol';
+import 'maci-contracts/sol/gatekeepers/SignUpGatekeeper.sol';
+import 'maci-contracts/sol/initialVoiceCreditProxy/InitialVoiceCreditProxy.sol';
+
+contract ClrFundMACIFactory is OwnableUpgradeable, MACIParameters, MACISharedObjs {
+  // Constants
+  uint256 private constant STATE_TREE_BASE = 2;
+  uint256 private constant MESSAGE_TREE_BASE = 2;
+  uint256 private constant VOTE_OPTION_TREE_BASE = 5;
+
+  // State
+  TreeDepths public treeDepths;
+  BatchSizes public batchSizes;
+  MaxValues public maxValues;
+  SnarkVerifier public batchUstVerifier;
+  SnarkVerifier public qvtVerifier;
+  uint256 public signUpDuration;
+  uint256 public votingDuration;
+
+  // Events
+  event MaciParametersChanged();
+  event MaciDeployed(address _maci);
+
+  function init(
+    uint8 _stateTreeDepth,
+    uint8 _messageTreeDepth,
+    uint8 _voteOptionTreeDepth,
+    uint8 _tallyBatchSize,
+    uint8 _messageBatchSize,
+    SnarkVerifier _batchUstVerifier,
+    SnarkVerifier _qvtVerifier,
+    uint256 _signUpDuration,
+    uint256 _votingDuration
+  )
+    public
+  {
+    __Ownable_init();
+    _setMaciParameters(
+      _stateTreeDepth,
+      _messageTreeDepth,
+      _voteOptionTreeDepth,
+      _tallyBatchSize,
+      _messageBatchSize,
+      _batchUstVerifier,
+      _qvtVerifier,
+      _signUpDuration,
+      _votingDuration
+    );
+  }
+
+  function _setMaciParameters(
+    uint8 _stateTreeDepth,
+    uint8 _messageTreeDepth,
+    uint8 _voteOptionTreeDepth,
+    uint8 _tallyBatchSize,
+    uint8 _messageBatchSize,
+    SnarkVerifier _batchUstVerifier,
+    SnarkVerifier _qvtVerifier,
+    uint256 _signUpDuration,
+    uint256 _votingDuration
+  )
+    internal
+  {
+    treeDepths = TreeDepths(_stateTreeDepth, _messageTreeDepth, _voteOptionTreeDepth);
+    batchSizes = BatchSizes(_tallyBatchSize, _messageBatchSize);
+    maxValues = MaxValues(
+      STATE_TREE_BASE ** treeDepths.stateTreeDepth - 1,
+      MESSAGE_TREE_BASE ** treeDepths.messageTreeDepth - 1,
+      VOTE_OPTION_TREE_BASE ** treeDepths.voteOptionTreeDepth - 1
+    );
+    batchUstVerifier = _batchUstVerifier;
+    qvtVerifier = _qvtVerifier;
+    signUpDuration = _signUpDuration;
+    votingDuration = _votingDuration;
+  }
+
+  /**
+    * @dev Set MACI parameters.
+    */
+  function setMaciParameters(
+    uint8 _stateTreeDepth,
+    uint8 _messageTreeDepth,
+    uint8 _voteOptionTreeDepth,
+    uint8 _tallyBatchSize,
+    uint8 _messageBatchSize,
+    SnarkVerifier _batchUstVerifier,
+    SnarkVerifier _qvtVerifier,
+    uint256 _signUpDuration,
+    uint256 _votingDuration
+  )
+    external
+    onlyOwner
+  {
+    require(
+      _voteOptionTreeDepth >= treeDepths.voteOptionTreeDepth,
+      'MACIFactory: Vote option tree depth can not be decreased'
+    );
+    _setMaciParameters(
+      _stateTreeDepth,
+      _messageTreeDepth,
+      _voteOptionTreeDepth,
+      _tallyBatchSize,
+      _messageBatchSize,
+      _batchUstVerifier,
+      _qvtVerifier,
+      _signUpDuration,
+      _votingDuration
+    );
+    emit MaciParametersChanged();
+  }
+
+  /**
+    * @dev Deploy new MACI instance.
+    */
+  function deployMaci(
+    SignUpGatekeeper _signUpGatekeeper,
+    InitialVoiceCreditProxy _initialVoiceCreditProxy,
+    address _coordinator,
+    PubKey calldata _coordinatorPubKey
+  )
+    external
+    onlyOwner
+    returns (MACI _maci)
+  {
+    _maci = new MACI(
+      treeDepths,
+      batchSizes,
+      maxValues,
+      _signUpGatekeeper,
+      batchUstVerifier,
+      qvtVerifier,
+      signUpDuration,
+      votingDuration,
+      _initialVoiceCreditProxy,
+      _coordinatorPubKey,
+      _coordinator
+    );
+    emit MaciDeployed(address(_maci));
+  }
+}
