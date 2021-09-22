d3a824e3dfb7eae39818bd1035dd48ccb161aa3e
Sam Richards <sbrichards@gmail.com>
2021-09-22 15:16:47 -0700

    Add sybil resistance content

--- vue-app/src/views/AboutSybilResistance.vue
+++ vue-app/src/views/AboutSybilResistance.vue
@@ -2,52 +2,62 @@
   <div class="about">
     <h1 class="content-heading">About Sybil Resistance</h1>
 
-    <h2>What is Sybil resistance?</h2>
+    <h2>What are Sybil attacks?</h2>
     <p>
-      <b
-        >tl;dr: if you want to create loads of accounts to vote for your
-        favourite projects multiple times, you're ngmi (not going to make it)</b
-      >
+      A "sybil attack" is when one person pretends to be many people in order to
+      subvert a system's rules. The more a system depends on each participant
+      being a single, unique person, the more potentially vulnerable it is to
+      sybil attacks. Social media is somewhat vulnerable: bots creating email
+      spam and fake follows or likes are examples of relatively toothless sybil
+      attacks. Digital voting systems are more vulnerable: if an attacker can
+      create enough fake users to swing a vote, there can be significant and
+      concrete consequences. Since clr.fund uses
+      <links to="/about/quadratic-funding">quadratic funding</links>, which is
+      essentially a voting system, sybil attacks are a serious threat.
     </p>
-    <h2>What is BrightID?</h2>
     <p>
-      We use BrightID to protect the funding round against sybil attacks. To
-      understand why this is important let's recap quadratic funding...
+      In the physical world, sybil attacks are hard to pull off. It's pretty
+      difficult to pretend to be someone else for long, especially when most
+      governments issue and check photo IDs and ID numbers. On the internet,
+      though, nobody knows you're a dog. People don't want to share their
+      personal information online and government IDs are impractical for
+      borderless web services, so historically, we haven't had strong methods to
+      stop online sybil attacks.
     </p>
 
-    <h2>Quadratic funding</h2>
+    <h2>Sybil Resistance and BrightID</h2>
     <p>
-      Quadratic funding is a democratic form of funding goods/services. A pot of
-      money, known as the matching pool, is distributed to all the projects
-      based on a formula. In the simplest terms, a project with 10 donations of
-      1 Dai could get more from the matching pool than a project with a single
-      100 Dai donation. The idea is that the projects with the most value to the
-      most amount of people (and not necessarily the wealthiest people) gets the
-      best funding.
+      BrightID is a platform we use to increase our confidence that you're only
+      contributing once. BrightID is one of a new class of digital identity
+      solutions for use cases like clr.fund that require digital sybil
+      resistance.
     </p>
     <p>
-      This falls apart if anyone can create multiple accounts and spam-donate to
-      a project. This is known as a sybil attack. To keep things fair we have to
-      be confident that you as an individual only contributed once. And that you
-      didn't create multiple Ethereum wallets and contribute from them all. This
-      is where BrightID steps in.
+      BrightID works using a web-of-trust model: users connect their BrightID
+      accounts with people they trust to be unique humans (generally because
+      they know them personally) and form groups. These connections are public,
+      but users' personal data aren't. If you connect your BrightID to an app,
+      the app can analyze your connections and decide whether or not to allow
+      you access based on how likely you are to be a unique person. The graph of
+      BrightID connections can be analyzed with any method, but generally, the
+      more connections an ID has with other trustworthy-looking IDs, the more
+      trustworthy it is.
     </p>
-    <h2>How BrightID helps</h2>
     <p>
-      BrightID is a product we use to increase our confidence that you're only
-      contributing once. Before you contribute, you must link your connected
-      wallet to your BrightID profile on your phone and register for the round
-      via a transaction. You can't link multiple wallets to the same BrightID
-      profile. This prevents you from creating 1000s of Ethereum accounts and
-      distorting the contributions.
+      To contribute to projects in a clr.fund round, you must connect a verified
+      BrightID with the clr.fund app. This prevents you from creating 1000s of
+      Ethereum accounts and distorting the contributions.
     </p>
+    <p>
+      <links to="/verify">Get verified by following the directions here</links>.
+    </p>
+
     <h2>How to set up BrightID</h2>
     <p>
-      The app will walk you through getting set up, but here's a quick out line
-      of what you can expect. If you've contributed to a clr.fund round before
-      or used BrightID with Gitcoin grants your experience will be shorter.
+      The app walks you through <links to="/verify">getting set up</links>, but
+      here's a quick look at what you can expect:
     </p>
-    <ul>
+    <ol>
       <li>
         Download BrightID for
         <links to="https://apps.apple.com/us/app/brightid/id1428946820"
@@ -60,24 +70,27 @@
       </li>
       <li>
         Link your wallet to your BrightID profile, so you can't use multiple
-        wallets to sway voting.
+        Ethereum addresses to sway voting.
       </li>
+      <li>Get verified in BrightID so we know you're a human and not a bot.</li>
       <li>
-        Get BrightID verified so BrightID knows you're a human and not a bot.
-      </li>
-      <li>
-        Pay a small transaction fee to cover BrightID's costs, also known as
-        sponsorship.
+        Submit a transaction for clr.fund to sponsor you in BrightID
         <links
           to="https://medium.com/brightid/brightid-sponsorships-5327a8d39f1e"
           >More on BrightID sponsorship</links
         >
       </li>
       <li>
-        Add yourself to the approved user registry. This is a requirement for
+        Add yourself to the clr.fund user registry. This is a requirement for
         <links to="/about/maci">MACI, our anti-bribery tech</links>.
       </li>
-    </ul>
+    </ol>
+    <p>
+      Note that you should only link one Ethereum address to your BrightID
+      profile. If you repeat our verification process and add another Ethereum
+      address, all contributions from you previous address will be ignored by
+      the matching pool. This is again to avoid sybil attacks.
+    </p>
   </div>
 </template>
 
