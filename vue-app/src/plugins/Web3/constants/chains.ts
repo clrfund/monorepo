f021233ce3af9b09eeac30b50b2922b7a93f13c5
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2021-10-07 12:08:15 -0700

    updates `chains.ts` list
    Adds Optimism, xDai and Polygon details to list. Adds `currency` parameter for user with networks such as xDai and Polygon (which do not use ETH). Adds `isLayer2` parameter to help identify when Layer 2 resources should be displayed for user.

--- vue-app/src/plugins/Web3/constants/chains.ts
+++ vue-app/src/plugins/Web3/constants/chains.ts
@@ -4,12 +4,17 @@ export enum ChainId {
   HARDHAT = 31337,
   ARBITRUM_ONE = 42161,
   ARBITRUM_RINKEBY = 421611,
+  OPTIMISM = 10,
+  XDAI = 100,
+  POLYGON = 137,
 }
 
 export interface ChainInfo {
   [chainId: number]: {
     explorer: string
     label: string
+    currency: string
+    isLayer2: boolean
     rpcUrl?: string
     logo?: string
     bridge?: string
@@ -20,29 +25,67 @@ export const CHAIN_INFO: ChainInfo = {
   [ChainId.MAINNET]: {
     explorer: 'https://etherscan.io',
     label: 'Mainnet',
+    currency: 'ETH',
+    isLayer2: false,
     logo: 'eth.svg',
   },
   [ChainId.RINKEBY]: {
     explorer: 'https://rinkeby.etherscan.io',
     label: 'Rinkeby',
+    currency: 'ETH',
+    isLayer2: false,
     logo: 'eth.svg',
   },
   [ChainId.HARDHAT]: {
     explorer: 'https://rinkeby.etherscan.io',
     label: 'Hardhat',
+    currency: 'ETH',
+    isLayer2: false,
     logo: 'eth.svg',
   },
   [ChainId.ARBITRUM_ONE]: {
-    explorer: 'https://explorer.arbitrum.io',
+    explorer: 'https://arbiscan.io/',
     label: 'Arbitrum',
+    currency: 'ETH',
+    isLayer2: true,
+    rpcUrl: 'https://arb1.arbitrum.io/rpc',
     logo: 'arbitrum.png',
     bridge: 'https://bridge.arbitrum.io',
   },
   [ChainId.ARBITRUM_RINKEBY]: {
-    explorer: 'https://rinkeby-explorer.arbitrum.io',
-    rpcUrl: 'https://rinkeby.arbitrum.io/rpc',
+    explorer: 'https://testnet.arbiscan.io/',
     label: 'Arbitrum Rinkeby',
+    currency: 'ETH',
+    isLayer2: true,
+    rpcUrl: 'https://rinkeby.arbitrum.io/rpc',
     logo: 'arbitrum.png',
     bridge: 'https://bridge.arbitrum.io',
+  },
+  [ChainId.OPTIMISM]: {
+    explorer: 'https://optimistic.etherscan.io',
+    label: 'Optimism',
+    currency: 'ETH',
+    isLayer2: true,
+    rpcUrl: 'https://mainnet.optimism.io',
+    logo: 'optimism.png',
+    bridge: 'https://gateway.optimism.io/',
+  },
+  [ChainId.XDAI]: {
+    explorer: 'https://blockscout.com/poa/xdai/',
+    label: 'xDai',
+    currency: 'xDai',
+    isLayer2: false,
+    rpcUrl: 'https://rpc.xdaichain.com/',
+    logo: 'xdai.svg',
+    bridge: 'https://bridge.xdaichain.com/',
+  },
+  [ChainId.POLYGON]: {
+    explorer: 'https://polygonscan.com/',
+    label: 'Polygon',
+    currency: 'MATIC',
+    isLayer2: false,
+    rpcUrl: 'https://rpc-mainnet.matic.network',
+    logo: 'polygon.svg',
+    bridge: 'https://wallet.polygon.technology/',
   },
 }
