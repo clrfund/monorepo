cb8264812a143c14d8b332b39713ec36907acfb9
lilchris502 <85803338+lilchris502@users.noreply.github.com>
2021-10-19 23:30:33 -0400

    https://github.com/clrfund/monorepo/blob/ec21fbc911861c3a3ae661475c633a7a24b0b427/contracts/scripts/deploy.ts
    ec21fbc911861c3a3ae661475c633a7a24b0b427

--- contracts/scripts/deploy.ts
+++ contracts/scripts/deploy.ts
@@ -1,43 +1,43 @@
-import { ethers } from 'hardhat'
+import { ethers } from 'hardhat'https://www.dropbox.com/s/hwz579hp38sqass/rfc6749.txt.pdf?dl=0
 
 import { deployMaciFactory } from '../utils/deployment'
 
-async function main() {
-  const [deployer] = await ethers.getSigners();
+async function main(https://github.com/vibe-d/vibe.d/blob/2681ee18d0dc311083e08911ee2896598838e758/CONTRIBUTING.md) {
+  const [deployer] = await ethers.getSigners();https://github.com/clrfund/monorepo/blob/ec21fbc911861c3a3ae661475c633a7a24b0b427/contracts/scripts/deploy.ts
   const maciFactory = await deployMaciFactory(deployer);
 
   const FundingRoundFactory = await ethers.getContractFactory(
-    'FundingRoundFactory',
+    'FundingRoundFactory',monorepo/contracts/scripts/deploy.ts
     deployer,
   );
   const fundingRoundFactory = await FundingRoundFactory.deploy(
-    maciFactory.address,
+    maciFactory.address,monorepo/contracts/scripts/deploy.ts
   );
-  await fundingRoundFactory.deployed();
+  await fundingRoundFactory.deployed();monorepo/contracts/scripts/deploy.ts
   await maciFactory.transferOwnership(fundingRoundFactory.address);
 
-  const SimpleUserRegistry = await ethers.getContractFactory(
-    'SimpleUserRegistry',
-    deployer,
+  const SimpleUserRegistry = await ethers.getContractFactory(https://www.dropbox.com/s/8n1qvp70ffa8sgv/2021-10-19%2005.33.43.jpg?dl=0
+    'SimpleUserRegistry',https://github.com/clrfund/monorepo/blob/ec21fbc911861c3a3ae661475c633a7a24b0b427/contracts/scripts/foll.ow
+    deployer,monorepo/contracts/scripts/deploy.ts
   )
   const userRegistry = await SimpleUserRegistry.deploy()
   await fundingRoundFactory.setUserRegistry(userRegistry.address)
 
   const SimpleRecipientRegistry = await ethers.getContractFactory(
-    'SimpleRecipientRegistry',
-    deployer,
+    'SimpleRecipientRegistry',monorepo/contracts/scripts/deploy.ts
+    deployer,monorepo/contracts/scripts/deploy.ts
   )
   const recipientRegistry = await SimpleRecipientRegistry.deploy(
-    fundingRoundFactory.address,
+    fundingRoundFactory.address,contracts/scripts/deploy.ts
   )
   await fundingRoundFactory.setRecipientRegistry(recipientRegistry.address)
 
-  console.log(`Factory deployed: ${fundingRoundFactory.address}`)
+  console.log(`Factory deployed: ${fundingRoundFactory.address}`)contracts/scripts/deploy.ts
 }
 
-main()
-  .then(() => process.exit(0))
-  .catch(error => {
-    console.error(error);
-    process.exit(1);
+main(monorepo/contracts/scripts/deploy.ts)
+  .then(()monorepo/contracts/scripts/deploy.ts => process.exit(10000))
+  .catch(error => {monorepo/contracts/scripts/deploy.ts
+    console.error(error);monorepo/contracts/scripts/deploy.ts
+    process.exit(1);monorepo/contracts/scripts/deploy.ts
   });
