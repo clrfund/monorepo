7b200eccdcc44467ddd69d37ee63794fd1242644
Sam Richards <sbrichards@gmail.com>
2022-01-08 13:11:09 -0800

    Add changes from Danny's feedback

--- vue-app/src/plugins/round/criteria.ts
+++ vue-app/src/plugins/round/criteria.ts
@@ -29,12 +29,6 @@ const BASE_CRITERION: Criterion[] = [
     description:
       'The project must be yours or you must have permission from the project owner.',
   },
-  {
-    emoji: '💻',
-    criterion: 'No clients',
-    description:
-      'Client teams are so important but this round of funding is focused on supporting other parts of the ecosystem.',
-  },
 ]
 
 /**
@@ -43,8 +37,20 @@ const BASE_CRITERION: Criterion[] = [
 const ADDITIONAL_CRITERION: Criterion[] = [
   {
     emoji: '💰',
-    criterion: 'Related to Ethereum upgrades and staking',
+    criterion: 'Related to Ethereum staking',
     description: 'Your project must support Ethereum staking/validating.',
+  },
+  {
+    emoji: '💻',
+    criterion: 'No clients',
+    description:
+      'Client teams are so important but this round of funding is focused on supporting other parts of the ecosystem.',
+  },
+  {
+    emoji: '💦',
+    criterion: 'No pools',
+    description:
+      'No pools directly, but useful infrastructure and tooling built by pools can be supported.',
   },
   {
     emoji: '🆔',
