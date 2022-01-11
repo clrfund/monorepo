26bd0285485fb8214d9d79e2c23573f99091474b
Sam Richards <sbrichards@gmail.com>
2022-01-11 13:51:17 -0800

    Simplify round criteria

--- vue-app/src/plugins/round/criteria.ts
+++ vue-app/src/plugins/round/criteria.ts
@@ -9,19 +9,13 @@ const BASE_CRITERION: Criterion[] = [
     emoji: '🤲',
     criterion: 'Free and open source',
     description:
-      'Your project code must be available to anyone to use under an open source license.',
+      'Your project code must be available to anyone to use under a free and open source license.',
   },
   {
     emoji: '👺',
     criterion: 'No scams',
     description:
       "Obviously, your project must not put anyone's funds or information at risk.",
-  },
-  {
-    emoji: '👯‍♀️',
-    criterion: 'No clones',
-    description:
-      "If you've forked code, you must provide additional, unique value to the ecosystem.",
   },
   {
     emoji: '🙋‍♀️',
@@ -39,18 +33,6 @@ const ADDITIONAL_CRITERION: Criterion[] = [
     emoji: '💰',
     criterion: 'Related to Ethereum staking',
     description: 'Your project must support Ethereum staking/validating.',
-  },
-  {
-    emoji: '💻',
-    criterion: 'No clients',
-    description:
-      'Client teams are so important but this round of funding is focused on supporting other parts of the ecosystem.',
-  },
-  {
-    emoji: '💦',
-    criterion: 'No pools',
-    description:
-      'No pools directly, but useful infrastructure and tooling built by pools can be supported.',
   },
   {
     emoji: '🆔',
