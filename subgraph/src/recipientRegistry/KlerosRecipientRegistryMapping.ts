64d85eda56645adb0760d0f2f5320fb39f245dd3
yuetloo <contact@yuetloo.com>
2022-03-07 16:25:08 -0500

    added SimpleRecipientRegistry and KlerosRecipientRegistry mapping

---
+++ subgraph/src/recipientRegistry/KlerosRecipientRegistryMapping.ts
@@ -0,0 +1,29 @@
+import {
+    RecipientAdded,
+    RecipientRemoved,
+} from '../../generated/templates/KlerosRecipientRegistry/KlerosRecipientRegistry'
+
+import { Recipient } from '../../generated/schema'
+import { removeRecipient } from '../RecipientMapping'
+
+export function handleRecipientAdded(event: RecipientAdded): void {
+    let recipientRegistryId = event.address.toHexString()
+
+    let recipientId = event.params._tcrItemId.toHexString()
+    let recipient = new Recipient(recipientId)
+    recipient.recipientRegistry = recipientRegistryId
+    recipient.createdAt = event.block.timestamp.toString()
+    recipient.recipientIndex = event.params._index
+    recipient.recipientMetadata = event.params._metadata.toHexString()
+
+    // TODO map recipient address from event.params._metadata
+    // recipient.recipientAddress = event.params._metadata
+    
+    recipient.save()
+}
+
+export function handleRecipientRemoved(event: RecipientRemoved): void {
+    let id = event.params._tcrItemId.toHexString()
+    let timestamp = event.block.timestamp.toString()
+    removeRecipient(id, timestamp)
+}
