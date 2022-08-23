5c967cb80b7ab8e3972e1129388316b2c57f6983
Pedro Kretzschmar <pedro.kretz@gmail.com>
2022-08-23 10:55:39 -0300

    hotfix: added new criterias

--- vue-app/src/plugins/round/criteria.ts
+++ vue-app/src/plugins/round/criteria.ts
@@ -6,28 +6,39 @@ export interface Criterion {
 
 const CRITERIA: Criterion[] = [
   {
-    emoji: '🤲',
+    emoji: '💰',
+    criterion: 'Related to Buidl Honduras',
+    description: 'Your project must support the Honduras ecosystem.',
+  },
+  {
+    emoji: '😺',
     criterion: 'Free and open source',
     description:
-      'Your project must be free to use and any code associated with your project should be available to anyone under an open source license.',
-  },
-  {
-    emoji: '👯‍♀️',
-    criterion: 'No clones',
-    description:
-      "If you've forked code, you must provide additional, unique value to the ecosystem.",
-  },
-  {
-    emoji: '🙋‍♀️',
-    criterion: 'Project ownership',
-    description:
-      'The project you submit must be yours or you must have permission from the project owner.',
+      'Your project code must be available to anyone to use under an open source license.',
   },
   {
     emoji: '👺',
     criterion: 'No scams',
     description:
       "Obviously, your project must not put anyone's funds or information at risk.",
+  },
+  {
+    emoji: '💀',
+    criterion: 'No clones',
+    description:
+      "If you've forked code, you must provide additional, unique value to the ecosystem.",
+  },
+  {
+    emoji: '🤓',
+    criterion: 'Project ownership',
+    description:
+      'The project must be yours or you must have permission from the project owner.',
+  },
+  {
+    emoji: '💻',
+    criterion: 'No vaporware',
+    description:
+      'Vaporware is important (?) but we will not be focusing on this.',
   },
 ]
 
