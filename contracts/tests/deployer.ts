6002ce334422567e86157858b5978218d422d047
yuetloo <contact@yuetloo.com>
2022-09-09 14:02:26 -0400

    adjust gas usage for deploying funding round in test cases

--- contracts/tests/deployer.ts
+++ contracts/tests/deployer.ts
@@ -267,7 +267,7 @@ describe('Clr fund deployer', () => {
       const deployed = factory.deployNewRound()
       await expect(deployed).to.emit(factory, 'RoundStarted')
       const deployTx = await deployed
-      expect(await getGasUsage(deployTx)).lessThan(12000000)
+      expect(await getGasUsage(deployTx)).lessThan(13000000)
 
       const fundingRoundAddress = await factory.getCurrentRound()
       expect(fundingRoundAddress).to.properAddress
--- contracts/tests/factory.ts
+++ contracts/tests/factory.ts
@@ -211,7 +211,7 @@ describe('Funding Round Factory', () => {
       const deployed = factory.deployNewRound()
       await expect(deployed).to.emit(factory, 'RoundStarted')
       const deployTx = await deployed
-      expect(await getGasUsage(deployTx)).lessThan(12000000)
+      expect(await getGasUsage(deployTx)).lessThan(13000000)
 
       const fundingRoundAddress = await factory.getCurrentRound()
       expect(fundingRoundAddress).to.properAddress
