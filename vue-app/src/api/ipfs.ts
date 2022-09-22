4c2f7d3dd095a18c1e105e79b530e5246a7edbe1
yuetloo <contact@yuetloo.com>
2022-09-22 13:31:33 -0400

    use pinata api key and secret for authentication

--- vue-app/src/api/ipfs.ts
+++ vue-app/src/api/ipfs.ts
@@ -1,23 +1,44 @@
-import { ipfsPinningUrl, ipfsPinningJwt } from './core'
+import {
+  ipfsPinningUrl,
+  ipfsPinningJwt,
+  ipfsApiKey,
+  ipfsSecretApiKey,
+} from './core'
 
 export class IPFS {
   url: string
   jwt: string
+  apiKey: string
+  secretApiKey: string
 
   constructor() {
     this.url = ipfsPinningUrl || ''
     this.jwt = ipfsPinningJwt || ''
+    this.apiKey = ipfsApiKey || ''
+    this.secretApiKey = ipfsSecretApiKey || ''
   }
 
   async add(file: File): Promise<string> {
     const data = new FormData()
     data.append('file', file)
 
+    const headers: Record<string, string> = {}
+
+    if (this.jwt) {
+      headers['Authorization'] = `Bearer ${this.jwt}`
+    }
+
+    if (this.apiKey) {
+      headers['pinata_api_key'] = this.apiKey
+    }
+
+    if (this.secretApiKey) {
+      headers['pinata_secret_api_key'] = this.secretApiKey
+    }
+
     const options = {
       method: 'POST',
-      headers: {
-        Authorization: `Bearer ${this.jwt}`,
-      },
+      headers,
       body: data,
     }
 
