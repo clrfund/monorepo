3c65c9b3c39f4fa1f6de3489b78e1670ffaaf3a2
yuetloo <contact@yuetloo.com>
2022-11-16 10:56:22 -0500

    add missing component

---
+++ vue-app/src/components/ErrorModal.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <div class="modal-body">
+      <div class="top">
+        <p>Error</p>
+        <button class="close-button" @click="$emit('close')">
+          <img class="pointer" src="@/assets/close.svg" />
+        </button>
+      </div>
+      <div class="error">{{ message }}</div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+// Libraries
+import Vue from 'vue'
+import { Prop } from 'vue-property-decorator'
+import Component from 'vue-class-component'
+
+@Component
+export default class ErrorModal extends Vue {
+  @Prop() message!: string // Required: error message to display
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../styles/vars';
+@import '../styles/theme';
+
+.vm--modal {
+  background-color: transparent !important;
+  box-shadow: none;
+}
+
+.modal-body {
+  margin-top: $modal-space;
+  text-align: left;
+  background: var(--bg-secondary-color);
+  border-radius: 1rem;
+  display: grid;
+  grid-gap: 10px;
+}
+
+.top {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: -16px;
+}
+
+.close-button {
+  background: transparent;
+  border: none;
+  img {
+    filter: var(--img-filter, invert(0.3));
+  }
+}
+</style>
