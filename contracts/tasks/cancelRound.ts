5b098e066e125fe054c14edbf2e2c9f89f8e8f3d
yuetloo <contact@yuetloo.com>
2022-12-06 17:20:41 -0500

    add cancel, finalize, new round github workflow with manual trigger

---
+++ contracts/tasks/cancelRound.ts
@@ -0,0 +1,17 @@
+import { task } from 'hardhat/config'
+
+task('cancel-round', 'Cancel the current round')
+  .addParam('factory', 'The funding round factory contract address')
+  .setAction(async ({ factory }, { network, ethers }) => {
+    const [deployer] = await ethers.getSigners()
+    console.log('caller', deployer)
+    const fundingRoundFactory = await ethers.getContractAt(
+      'FundingRoundFactory',
+      factory,
+      deployer
+    )
+
+    const cancelTx = await fundingRoundFactory.cancelCurrentRound()
+    await cancelTx.wait()
+    console.log('Cancel transaction hash: ', cancelTx.hash)
+  })
