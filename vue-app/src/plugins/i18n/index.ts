9ec10b9474611d2c20ec07a274da62b215f10e0a
yuetloo <contact@yuetloo.com>
2023-05-04 13:43:15 -0400

    add italian transaction

--- vue-app/src/plugins/i18n/index.ts
+++ vue-app/src/plugins/i18n/index.ts
@@ -1,9 +1,11 @@
 import { createI18n } from 'vue-i18n'
 import cn from '@/locales/cn.json'
 import en from '@/locales/en.json'
+import it from '@/locales/it.json'
 
 const defaultLocale = import.meta.env.VITE_I18N_LOCALE || 'en'
 export const languages = [
+  { locale: 'it', emoji: '🇮🇹', description: 'Italiana' },
   { locale: 'en', emoji: '🇺🇸', description: 'English' },
   { locale: 'cn', emoji: '🇨🇳', description: '简体中文' },
   /*
@@ -19,12 +21,13 @@ export function isLocaleSupported(locale) {
 }
 
 type MessageSchema = typeof en
-const i18n = createI18n<[MessageSchema], 'cn' | 'en'>({
+const i18n = createI18n<[MessageSchema], 'it' | 'cn' | 'en'>({
   legacy: false,
   locale: defaultLocale,
   fallbackLocale: defaultLocale,
   globalInjection: true,
   messages: {
+    it,
     cn,
     en,
   },
