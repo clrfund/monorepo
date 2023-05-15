453388dd9a3c5a4a9ef28b4477b98a0e4fde2198
yuetloo <contact@yuetloo.com>
2023-05-15 12:01:46 -0400

    refactor valid signature check

--- vue-app/src/stores/user.ts
+++ vue-app/src/stores/user.ts
@@ -4,8 +4,7 @@ import { useAppStore } from '@/stores'
 import type { WalletUser } from '@/stores'
 import { getContributionAmount, hasContributorVoted } from '@/api/contributions'
 import type { BigNumber, Signer } from 'ethers'
-import { utils } from 'ethers'
-import { ensLookup } from '@/utils/accounts'
+import { ensLookup, isValidSignature } from '@/utils/accounts'
 import { UserRegistryType, userRegistryType } from '@/api/core'
 import { getBrightId, type BrightId } from '@/api/bright-id'
 import { assert, ASSERT_NOT_CONNECTED_WALLET } from '@/utils/assert'
@@ -46,7 +45,7 @@ export const useUserStore = defineStore('user', {
       if (!this.currentUser.encryptionKey) {
         const signature = await this.signer.signMessage(LOGIN_MESSAGE)
 
-        if (!utils.isHexString(signature)) {
+        if (!isValidSignature(signature)) {
           // gnosis safe does not return signature in hex string
           // show the result as error
           throw new Error(signature)
