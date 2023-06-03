1009079ec5b13856d9a4f534e52643fdfa9ea342
yuetloo <contact@yuetloo.com>
2023-06-02 23:24:44 -0400

    fix missing argument error

--- vue-app/src/api/leaderboard.ts
+++ vue-app/src/api/leaderboard.ts
@@ -1,9 +1,18 @@
 import leaderboardRounds from '@/rounds/rounds.json'
 
-export async function getLeaderboardData(roundAddress: string) {
-  const found = leaderboardRounds.find(record => {
-    return record.address === roundAddress
+type LeaderboardRecord = {
+  address: string
+  network: string
+}
+
+export async function getLeaderboardData(roundAddress: string, network: string) {
+  const rounds = leaderboardRounds as LeaderboardRecord[]
+
+  const lowercaseRoundAddress = roundAddress.toLocaleLowerCase()
+  const lowercaseNetwork = network.toLowerCase()
+  const found = rounds.find((r: LeaderboardRecord) => {
+    return r.address.toLowerCase() === lowercaseRoundAddress && r.network.toLowerCase() === lowercaseNetwork
   })
 
-  return found ? import(`../rounds/${found.network}/${found.address}`) : null
+  return found ? import(`../rounds/${found.network}/${found.address}.json`) : null
 }
