71e87e670c809509f7a5418f8ce16f0a50eaab66
yuetloo <contact@yuetloo.com>
2023-06-06 08:05:09 -0400

    add spanish to dropdown menu

--- vue-app/src/plugins/i18n/index.ts
+++ vue-app/src/plugins/i18n/index.ts
@@ -1,14 +1,15 @@
 import { createI18n } from 'vue-i18n'
 import cn from '@/locales/cn.json'
 import en from '@/locales/en.json'
+import es from '@/locales/es.json'
 
 const defaultLocale = import.meta.env.VITE_I18N_LOCALE || 'en'
 export const languages = [
   { locale: 'en', emoji: '🇺🇸', description: 'English' },
+  { locale: 'es', emoji: '🇪🇸', description: 'Español' },
   { locale: 'cn', emoji: '🇨🇳', description: '简体中文' },
   /*
   // comment out as we don't have the translations for these yet
-  { locale: 'es', emoji: '🇪🇸', description: 'Español'  },
   { locale: 'tw', emoji: '🇹🇼', description: '繁體中文' },
    */
 ]
@@ -19,13 +20,14 @@ export function isLocaleSupported(locale) {
 }
 
 type MessageSchema = typeof en
-const i18n = createI18n<[MessageSchema], 'cn' | 'en'>({
+const i18n = createI18n<[MessageSchema], 'cn' | 'es' | 'en'>({
   legacy: false,
   locale: defaultLocale,
   fallbackLocale: defaultLocale,
   globalInjection: true,
   messages: {
     cn,
+    es,
     en,
   },
 })
