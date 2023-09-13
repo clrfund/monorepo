3cbfb25dd2223e1bdc7942fbec0a8dc196302a38
yuetloo <contact@yuetloo.com>
2023-09-13 17:00:57 -0400

    fix missing dependencies

--- common/src/index.ts
+++ common/src/index.ts
@@ -1,7 +1,3 @@
-import { extractVk } from 'maci-circuits'
-import { VerifyingKey } from 'maci-domainobjs'
-import { genProcessVkSig, genTallyVkSig, genSubsidyVkSig } from 'maci-core'
-
 export * from './block'
 export * from './proof'
 export * from './merkle'
@@ -9,11 +5,3 @@ export * from './ipfs'
 export * from './keypair'
 export * from './tally'
 export * from './utils'
-
-export {
-  extractVk,
-  VerifyingKey,
-  genProcessVkSig,
-  genTallyVkSig,
-  genSubsidyVkSig,
-}
