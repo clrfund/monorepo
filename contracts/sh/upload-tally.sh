4930f8d20257acddd1c2c4c0a779edaf2e4bbf91
yuetloo <contact@yuetloo.com>
2024-01-02 14:52:43 -0500

    remove change directory in the script

--- contracts/sh/upload-tally.sh
+++ contracts/sh/upload-tally.sh
@@ -3,16 +3,14 @@ set -e
 
 
 # Upload the tally.json file in the contracts directory to IPFS
+# Run the script in the contracts directory as follow:
 #
-# Usage: ./upload-tally.sh
+# sh/upload-tally.sh
 #
 # Make sure the PINATA_JWT secret is set in the AWS secrets manager
 #   aws scretsmanager create-secret --name PINATA_JWT --secret-string "SECRET_VALUE"
 #
 
-CONTRACTS_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && cd .. && pwd )
-
-cd $CONTRACT_DIR
 PINATA_JWT=$(aws secretsmanager get-secret-value --secret-id PINATA_JWT | jq -r '.SecretString')
 curl --location --request POST 'https://api.pinata.cloud/pinning/pinFileToIPFS' \
           --header "Authorization: Bearer ${PINATA_JWT}" \
