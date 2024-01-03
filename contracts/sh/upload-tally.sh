ff0ba392f0ad0e489112447a62fafe8b77e5b99a
yuetloo <contact@yuetloo.com>
2024-01-02 19:37:39 -0500

    specify region

--- contracts/sh/upload-tally.sh
+++ contracts/sh/upload-tally.sh
@@ -7,11 +7,11 @@ set -e
 #
 # sh/upload-tally.sh
 #
-# Make sure the PINATA_JWT secret is set in the AWS secrets manager
+# Make sure the PINATA_JWT secret is set in the AWS secrets manager in the us-east-1 region
 #   aws scretsmanager create-secret --name PINATA_JWT --secret-string "SECRET_VALUE"
 #
 
-PINATA_JWT=$(aws secretsmanager get-secret-value --secret-id PINATA_JWT | jq -r '.SecretString')
+PINATA_JWT=$(aws secretsmanager get-secret-value --region us-east-1 --secret-id PINATA_JWT | jq -r '.SecretString')
 curl --location --request POST 'https://api.pinata.cloud/pinning/pinFileToIPFS' \
           --header "Authorization: Bearer ${PINATA_JWT}" \
           --form 'file=@"tally.json"'
