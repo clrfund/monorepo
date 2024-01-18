5b409a42b1829e76833ffc5212c35999e762835d
yuetloo <contact@yuetloo.com>
2024-01-18 15:27:44 -0500

    add missing constant

--- contracts/utils/constants.ts
+++ contracts/utils/constants.ts
@@ -4,6 +4,7 @@ export const UNIT = 10n ** 18n
 export const VOICE_CREDIT_FACTOR = 10n ** BigInt(4 + 18 - 9)
 export const ALPHA_PRECISION = 10n ** 18n
 export const DEFAULT_SR_QUEUE_OPS = '4'
+export const DEFAULT_GET_LOG_BATCH_SIZE = 20000
 
 export enum RecipientState {
   Registered = 'Registered',
