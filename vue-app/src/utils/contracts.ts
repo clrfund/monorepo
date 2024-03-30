62680933f352bbfc4cb6ca3140847573cba19c2c
yuetloo <contact@yuetloo.com>
2024-03-30 13:57:40 -0400

    do not wait for synchronization with subgraph

--- vue-app/src/utils/contracts.ts
+++ vue-app/src/utils/contracts.ts
@@ -47,39 +47,6 @@ export async function waitForTransaction(
   return transactionReceipt
 }
 
-/**
- * Wait for transaction to be mined and available on the subgraph
- * @param pendingTransaction transaction to wait and check for
- * @param checkFn the check function
- * @param onTransactionHash callback function with the transaction hash
- * @returns transaction receipt
- */
-export async function waitForTransactionAndCheck(
-  pendingTransaction: Promise<TransactionResponse>,
-  checkFn: (receipt: TransactionReceipt) => Promise<boolean>,
-  onTransactionHash?: (hash: string) => void,
-): Promise<TransactionReceipt> {
-  const receipt = await waitForTransaction(pendingTransaction, onTransactionHash)
-
-  return new Promise(resolve => {
-    async function checkAndWait(depth = 0) {
-      if (await checkFn(receipt)) {
-        resolve(receipt)
-      } else {
-        if (depth > MAX_WAIT_DEPTH) {
-          throw new Error('Time out waiting for transaction ' + receipt.hash)
-        }
-
-        const timeoutMs = 2 ** depth * 10
-        await new Promise(res => setTimeout(res, timeoutMs))
-        checkAndWait(depth + 1)
-      }
-    }
-
-    checkAndWait()
-  })
-}
-
 export async function isTransactionMined(hash: string): Promise<boolean> {
   const receipt = await provider.getTransactionReceipt(hash)
   return !!receipt
