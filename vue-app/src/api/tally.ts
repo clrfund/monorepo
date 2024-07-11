f374185cd2e41f48ad6d392e503a72d84bea33ee
yuetloo <contact@yuetloo.com>
2024-07-10 21:45:29 -0400

    fix merge error

--- vue-app/src/api/tally.ts
+++ vue-app/src/api/tally.ts
@@ -6,24 +6,33 @@ import { provider, chain } from './core'
 // https://github.com/webpack/webpack/issues/7378#issuecomment-683891615
 import type { Tally } from '@clrfund/common'
 import { getIpfsUrl } from '@/utils/url'
-import { getLeaderboardRoundInfo } from './round'
+import { findStaticRound } from './round'
 
 export { Tally }
 
-export async function getTally(fundingRoundAddress: string): Promise<Tally> {
-  const fundingRound = new Contract(fundingRoundAddress, FundingRound, provider)
-  const tallyHash = await fundingRound.tallyHash()
+export async function getTally(fundingRoundAddress: string): Promise<Tally | null> {
+  let tally: Tally | null = null
 
   try {
     // try to get the tally file statically first, if not found, try the IPFS gateway
-    const round = await getLeaderboardRoundInfo(fundingRoundAddress, chain.name)
+    const round = await findStaticRound(fundingRoundAddress, chain.name)
     if (round?.tally) {
-      return round.tally
-    } else {
-      throw new Error('No tally data, get from IPFS gateway')
+      tally = round.tally
     }
   } catch {
-    const response = await fetch(getIpfsUrl(tallyHash) || '')
-    return await response.json()
+    // ignore error and try to get the tally file from Ipfs gateway
   }
+
+  if (!tally) {
+    try {
+      const fundingRound = new Contract(fundingRoundAddress, FundingRound, provider)
+      const tallyHash = await fundingRound.tallyHash()
+      const response = await fetch(getIpfsUrl(tallyHash) || '')
+      return await response.json()
+    } catch {
+      // ignore error and return null
+    }
+  }
+
+  return tally
 }
